680d285385eefadf25299edff3ca1cab48aef961
Chris Waldron <chris@chrissolutions.com>
2016-03-31 21:23:20 +1300

    Modifed template behavior.

--- humdrum/js/humdrum.js
+++ humdrum/js/humdrum.js
@@ -5,7 +5,7 @@ var Humdrum = (function (hd) {
     "use strict";
     
     /**
-     * Javascript class inheritance.
+     * JavaScript class inheritance.
      *
      * This uses a technique of copying the prototype and properties of the base class
      * to the derive class.
@@ -79,7 +79,7 @@ var Humdrum = (function (hd) {
         }
         
         /**
-         * creates a class instance.
+         * creates an object instance of the class.
          *
          * @example
          * Myclass.create([arg1[, arg2[, ...]]]);
@@ -847,76 +847,160 @@ var Humdrum = (function (hd) {
      * @class
      */
     hd.Template = function() {
-        this.inherit(hd.Template);
+		this.inherit(hd.Template);
+		
+		var noop = function() {},
+			templateCache = Object.create(null),
+			isLoading = false;
+		
+		/**
+		 * Load the template cache from the source properties.
+		 *
+		 * @param {object} source      Template source object.
+		 */
+		function loadTemplateFromObject(source, callback) {
+			Object.getOwnPropertyNames(source).forEach(function(name) {
+				templateCache[name] = Handlebars.compile(source[name]);
+			});
+			
+			callback();
+		}
+		
+		/**
+		 * Load the template cache from the DOM.
+		 *
+		 * @param {jquery} source  DOM element containing templates.
+		 */
+		function loadTemplateFromElement(source, callback) {
+			source.children().each(function(index, element) {
+				var name = element.getAttribute('id').replace('template-', '');
+				templateCache[name] = Handlebars.compile(element.innerHTML);
+			});
+			
+			callback();
+		}
+		
+		/**
+		 * Retrieve templates from URL.
+		 *
+		 * @param {string} source  The URL of the templates.
+		 */
+		function loadTemplateFromUrl(source, callback) {
+			var lastSeparator = source.lastIndexOf('.'),
+				name = source.substr(0, lastSeparator),
+				ext = source.substr(lastSeparator);
+			
+			// start the loading.
+			isLoading = true;
+			
+			// load template file.
+			$.ajax({
+				url: name + (ext || '.html'),
+				dataType: 'text',
+				success: function(data) {
 
-        var templates = {};
-        var names = null;
+					// find the template section.
+					var templateSection = $('#template-section');
+					if (!templateSection.length) {
+						templateSection = $(document.createElement('section'));
+						templateSection.attr('id', 'template-section');
+					}
 
-        /**
-         * Retreives the template by name.
-         *
-         * @param {string} name    template name.
-         */
-        function getTemplate(name) {
-            if (templates[name] === undefined) {
-
-                // load undefined template.
-                // ReSharper disable once PossiblyUnassignedProperty
-                $.ajax({
-                    url: "templates/" + name + ".txt",
-                    success: function(data) {
-
-                        // compile and cache the template.
-                        // ReSharper disable once UndeclaredGlobalVariableUsing
-                        templates[name] = Handlebars.compile(data);
-                    },
-                    async: false
-                });
-            }
-
-            return templates[name];
-        }
-
-        /**
-         * Initialize the template
-         *
-         * @param {object} source  Template source object.
-         */
-        this.init = function(source) {
-            Object.getOwnPropertyNames(source).forEach(new hd.Subscriber(this, function(name) {
-                templates[name] = Handlebars.compile(source[name]);
-                Object.defineProperty(this, name, {
-                    get: function() { return name; },
-                    enumerable: true,
-                    configurable: false
-                });
-            }));
-        };
-
-        /**
-         * Create text using the named template.
-         *
-         * @param {string} name    Template name.
-         * @param {object} data    Template data.
-         */
-        this.createTextFor = function(name, data) {
-            if (!name) return;
-            var template = getTemplate(name);
-            return template(data);    
-        };
-
-        /**
-         * Create element using the named template.
-         *
-         * @param {string} name    Template name.
-         * @param {object} data    Template data.
-         */
-        this.createElementFor = function(name, data) {
-            var html = this.createTextFor(name, data);    
-            var d = document.createElement("div");
-            d.innerHTML = html;
-            return d.children[0];
-        };
+					templateSection.append($.parseHTML(data));
+					templateSection.children().each(function(index, element) {
+						var name = element.getAttribute('id').replace('template-', '');
+						templateCache[name] = Handlebars.compile(element.innerHTML);
+					});
+					
+					templateSection.empty();
+					isLoading = false;
+					callback();
+				}
+			});
+		}
+		
+		/**
+		 * Retrieve templates from URL.
+		 *
+		 * @param {$|HTMLElement|object|string}    source      Template source.
+		 * @param {function}                       callback    Loader callback.
+		 */
+		function loadTemplate(source, callback) {
+			
+			callback = callback || noop;
+			
+			if (source instanceof $) {
+				loadTemplateFromElement(source, callback);
+			} else if (source instanceof HTMLElement) {
+				loadTemplateFromElement($(source), callback);
+			} else if (typeof source === "string") {
+				loadTemplateFromUrl(source, callback);
+			} else {
+				loadTemplateFromObject(source, callback);
+			}
+		}
+		
+		/**
+		 * isLoading getter.
+		 *
+		 * @param {boolean} isLoading  Determines whether the template is loading.
+		 */
+		Object.defineProperty(this, 'isLoading', {
+			get: function() { return isLoading; },
+			enumerable: true,
+			configurable: false
+		});
+		
+		/**
+		 * Retrieves the template by name.
+		 *
+		 * @param {string} name    template name.
+		 */
+		function getTemplate(name) {
+			return templateCache[name];
+		}
+		
+		/**
+		 * Initialize the template
+		 *
+		 * @param {$|HTMLElement|object|string}    source      Template source.
+		 */
+		this.init = function(source) {
+			loadTemplate(source, hd.Subscriber(this, function() {
+				Object.getOwnPropertyNames(templateCache).forEach(hd.Subscriber(this, function(name) {
+					Object.defineProperty(this, name, {
+						get: function() { return name; },
+						enumerable: true,
+						configurable: false
+					});
+				}));
+			}));
+		};
+		
+		/**
+		 * Create text using the named template.
+		 *
+		 * @param {string} name    Template name.
+		 * @param {object} data    Template data.
+		 */
+		this.createTextFor = function(name, data) {
+			if (!name) return;
+			var template = getTemplate(name);
+			return template(data);    
+		};
+		
+		/**
+		 * Create element using the named template.
+		 *
+		 * @param {string} name    Template name.
+		 * @param {object} data    Template data.
+		 */
+		this.createElementFor = function(name, data) {
+			var html = this.createTextFor(name, data);    
+			var d = document.createElement("div");
+			d.innerHTML = html;
+			return d.children[0];
+		};
     };
     
     /**
